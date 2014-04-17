edb8a61a6b1d37ee198e31d0dc344eb8e3e11168
sudheerj <sudheer.jonna@gmail.com>
2014-04-18 00:59:00 +0530

    Lazyloading changes

--- chapter04/src/main/java/com/packt/pfblueprints/controller/LazyAccountSummaryDataModel.java
+++ chapter04/src/main/java/com/packt/pfblueprints/controller/LazyAccountSummaryDataModel.java
@@ -41,55 +41,6 @@ public class LazyAccountSummaryDataModel extends LazyDataModel<AccountSummary> {
  
     @Override
     public List<AccountSummary> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,Object> filters) {
-        List<AccountSummary> data = new ArrayList<AccountSummary>();
- 
-        //filter
-        for(AccountSummary accountSummaryObj : accountsInfo) {
-            boolean match = true;
- 
-            for(Iterator<String> it = filters.keySet().iterator(); it.hasNext();) {
-                try {
-                    String filterProperty = it.next();
-                    String filterValue = filters.get(filterProperty).toString();
-                    String fieldValue = String.valueOf(accountSummaryObj.getClass().getField(filterProperty).get(accountSummaryObj));
- 
-                    if(filterValue == null || fieldValue.startsWith(filterValue)) {
-                        match = true;
-                    }
-                    else {
-                        match = false;
-                        break;
-                    }
-                } catch(Exception e) {
-                    match = false;
-                } 
-            }
- 
-            if(match) {
-                data.add(accountSummaryObj);
-            }
-        }
- 
-        //sort
-        if(sortField != null) {
-           // Collections.sort(data, new LazySorter(sortField, sortOrder));
-        }
- 
-        //rowCount
-        int dataSize = data.size();
-        this.setRowCount(dataSize);
- 
-        //paginate
-        if(dataSize > pageSize) {
-            try {
-                return data.subList(first, first + pageSize);
-            }
-            catch(IndexOutOfBoundsException e) {
-                return data.subList(first, first + (dataSize % pageSize));
-            }
-        }
-        else {
-            return data;
-        }
+    	return new ArrayList<AccountSummary>();
     }
 }
