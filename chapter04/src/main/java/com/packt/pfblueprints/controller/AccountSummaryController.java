d9c416706b145db50ae031405e59cde19f233955
sudheerj <sudheer.jonna@gmail.com>
2014-04-19 19:52:10 +0530

    RTL support and code formatting

--- chapter04/src/main/java/com/packt/pfblueprints/controller/AccountSummaryController.java
+++ chapter04/src/main/java/com/packt/pfblueprints/controller/AccountSummaryController.java
@@ -21,61 +21,61 @@ import com.packt.pfblueprints.model.Dealer;
 
 @ManagedBean
 @ViewScoped
-public class AccountSummaryController  implements Serializable{
+public class AccountSummaryController implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	private List<AccountSummary> accountsInfo=new ArrayList<AccountSummary>();
-	
-	private AccountSummary accountobj=new AccountSummary();
+	private List<AccountSummary> accountsInfo = new ArrayList<AccountSummary>();
+
+	private AccountSummary accountobj = new AccountSummary();
 	AccountsDAO dao = new AccountsDAO();
 	private LazyDataModel<AccountSummary> lazyAccSummaryDataModel;
-	
+	private Boolean direction = false;
+	private String directionSupport = "ltr";
+
 	@PostConstruct
-	public void init() { 
-		
-		lazyAccSummaryDataModel = new LazyAccountSummaryDataModel(){
+	public void init() {
+
+		lazyAccSummaryDataModel = new LazyAccountSummaryDataModel() {
 			@Override
-			public List<AccountSummary> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,Object> filters) {
-				
-				String sortOrderValue=null;
-				if(sortField==null){
-					sortField="investorName";
+			public List<AccountSummary> load(int first, int pageSize,
+					String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+
+				String sortOrderValue = null;
+				if (sortField == null) {
+					sortField = "investorName";
 				}
-				if(sortOrder.ASCENDING.equals("A")){
-					sortOrderValue="ASC";
+				if (sortOrder.ASCENDING.equals("A")) {
+					sortOrderValue = "ASC";
+				} else if (sortOrder.DESCENDING.equals("D")) {
+					sortOrderValue = "DSC";
+				} else {
+					sortOrderValue = "default";
 				}
-				else if(sortOrder.DESCENDING.equals("D")){
-					sortOrderValue="DSC";
-				}
-				else{
-					sortOrderValue="default";
-				}
-				
-				accountsInfo=dao.getAllAccounts(first,pageSize,sortField,sortOrderValue,filters); 
+
+				accountsInfo = dao.getAllAccounts(first, pageSize, sortField,
+						sortOrderValue, filters);
 				this.setRowCount(20);
 				return accountsInfo;
 			}
 		};
-		
-	}
-	
-	/* public void deleteDealer(){
-		dealerInfo=dao.deleteDealer(dealerobj);
+
 	}
 
-	public dealer getDealerobj() {
-		return dealerobj;
-	} */
+	public void addRTLsupport() {
+		if (direction) {
+			directionSupport = "rtl";
+		}else{
+			directionSupport = "ltr";
+		}
+	}
 
-	
 	public LazyDataModel<AccountSummary> getLazyAccSummaryDataModel() {
 		return lazyAccSummaryDataModel;
 	}
 
-	
-
-	public void setLazyAccSummaryDataModel(LazyDataModel<AccountSummary> lazyAccSummaryDataModel) {
+	public void setLazyAccSummaryDataModel(
+			LazyDataModel<AccountSummary> lazyAccSummaryDataModel) {
 		this.lazyAccSummaryDataModel = lazyAccSummaryDataModel;
 	}
 
@@ -86,6 +86,21 @@ public class AccountSummaryController  implements Serializable{
 	public void setAccountobj(AccountSummary accountobj) {
 		this.accountobj = accountobj;
 	}
-	
-	
+
+	public Boolean getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Boolean direction) {
+		this.direction = direction;
+	}
+
+	public String getDirectionSupport() {
+		return directionSupport;
+	}
+
+	public void setDirectionSupport(String directionSupport) {
+		this.directionSupport = directionSupport;
+	}
+
 }
