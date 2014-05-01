0f25a2c4f9e0945f3480b5cb8f443f13cf7ecdc3
sudheerj <sudheer.jonna@gmail.com>
2014-05-01 21:12:24 +0530

    Product model DTO added

---
+++ chapter10/src/main/java/com/packt/pfblueprints/model/Product.java
@@ -0,0 +1,16 @@
+package com.packt.pfblueprints.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class Product {
+
+	
+}
